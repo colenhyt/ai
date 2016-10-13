ef4fdb5a3712d4a4888d58ddd6ae3ca286aa37f8
colenhyt <colen.hyt@gmail.com>
2016-10-13 17:19:10 +0800

    add

--- boxsite/bin/startup.bat
+++ boxsite/bin/startup.bat
@@ -24,9 +24,17 @@ for /f "delims=" %%a in ('dir /b "%BOXLIB_HOME%\mallet-2.0.6\lib\*.jar"') do (
 set "MALLET_LIB=!MALLET_LIB!;%BOXLIB_HOME%\mallet-2.0.6\lib\%%a"
 )
 
-echo %MALLET_LIB%
+set WEBMAGIC_LIB=
+for /f "delims=" %%a in ('dir /b "%BOXLIB_HOME%\webmagic\lib\*.jar"') do (
+set "WEBMAGIC_LIB=!WEBMAGIC_LIB!;%BOXLIB_HOME%\webmagic\lib\%%a"
+)
+
+rem echo %WEBMAGIC_LIB%
 
 set CLASSPATH=%BOXLIB_HOME%\webmagic\bin
+set "CLASSPATH=%CLASSPATH%;%JSO-COMMOM_LIB%"
+set "CLASSPATH=%CLASSPATH%;%WEBMAGIC_LIB%"
+set "CLASSPATH=%CLASSPATH%;%MALLET_LIB%"
 set "CLASSPATH=%CLASSPATH%;%BOXSITE_LIB%"
 set "CLASSPATH=%CLASSPATH%;%BOXLIB_HOME%\jso-common\bin"
 set "CLASSPATH=%CLASSPATH%;%BOXLIB_HOME%\mallet-2.0.6\class"
@@ -39,5 +47,5 @@ rem echo classpath:"%CLASSPATH%"
 
 set CLASS=box.mgr.ProcessManager
 
-rem java -Xmx%BOXSITE_MEMORY% -ea -Dfile.encoding=%BOXSITE_ENCODING% %CLASS%
+java -Xmx%BOXSITE_MEMORY% -ea -Dfile.encoding=%BOXSITE_ENCODING% %CLASS%
 
